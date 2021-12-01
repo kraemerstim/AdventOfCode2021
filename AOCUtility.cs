ef039077bb3026685f332ca9fd910ba84355ed68
Tim Kraemer <tim.kraemer@cas.de>
2021-12-01 12:07:50 +0100

    Finished day 1 solution

--- AOCUtility.cs
+++ AOCUtility.cs
@@ -1,17 +1,25 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 using System.Runtime.CompilerServices;
 
 namespace AdventOfCode2021
 {
     public static class AOCUtility
     {
-        public static IEnumerable<string> FileLines(string filename)
+        public static List<String> FileLines(string filename)
         {
-            string[] lines = System.IO.File.ReadAllLines(filename);
-            foreach (string line in lines)
+            try
             {
-                yield return line;
+                return System.IO.File.ReadAllLines(filename).ToList();
             }
+            catch
+            {
+                throw new FileNotFoundException($"File not found: {filename}");
+            }
+            
         }
     }
 }
--- Day1.cs
+++ Day1.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace AdventOfCode2021
 {
@@ -6,10 +10,15 @@ namespace AdventOfCode2021
     {
         public override void Run()
         {
-            foreach (var test in AOCUtility.FileLines(@"resources/day1/input.txt"))
-            {
-                Log(test);
-            }
+            List<int> depthList = AOCUtility.FileLines(@"resources/day1/input.txt").Select(int.Parse).ToList();
+
+            int depthCounter1 = Enumerable.Range(0, depthList.Count - 1)
+                .Count(x => (depthList[x] < depthList[x + 1]));
+            int depthCounter2 = Enumerable.Range(0, depthList.Count - 3)
+                .Count(x => (depthList[x] < depthList[x + 3]));
+
+            Log(Convert.ToString(depthCounter1), Main.LogLevel.Result1);
+            Log(Convert.ToString(depthCounter2), Main.LogLevel.Result2);
         }
     }
 }
